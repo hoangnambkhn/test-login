9feb2c66611d49e0f93fe30c88e242d3a4d1a1e5
hoangnambkhn <namhoang18595@gmail.com>
2018-07-19 14:00:51 +0700

    new code
    new code

--- src/container/login.js
+++ src/container/login.js
@@ -7,41 +7,45 @@ import Term from '../component/term';
 
 
 class login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            isShowSignup : false,
-            isShowTeacher : false,
-            isShowStudent : false,
-            isShowTeam : false,
-            name : ''
-
+            isShowSignup: false,
+            isShowTeacher: false,
+            isShowStudent: false,
+            isShowTeam: false,
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            isTeacher: false,
+            isStudent: false
         }
     }
     render() {
         return (
             <div>
-                <Header showPopupSignin = {(index) => this.setState({isShowSignup : index})} />
+                <Header showPopupSignin={(index) => this.setState({ isShowSignup: index })} />
                 {this.state.isShowSignup ?
-                    <Signin updateData = {(index1,index2,index3,index4,index5) => this.setState({
-                        isShowSignup : index1,
-                        isShowStudent : index3,
-                        isShowTeacher : index2,
-                        isShowTeam : index4,
-                        name : index5
-                    })}/>
+                    <Signin updateData={(index1, index2, index3, index4, index5) => this.setState({
+                        isShowSignup: index1,
+                        isShowStudent: index3,
+                        isShowTeacher: index2,
+                        isShowTeam: index4,
+                        firstName: index5
+                    })} />
                     : null
                 }
                 {this.state.isShowTeacher ?
-                    <TeacherForm name = {this.state.name} />
+                    <TeacherForm name={this.state.name} />
                     : null
                 }
                 {this.state.isShowStudent ?
-                    <StudentForm/>
+                    <StudentForm />
                     : null
                 }
                 {this.state.isShowSignup ?
-                    <Signin/>
+                    <Signin />
                     : null
                 }
             </div>
--- src/header/header.js
+++ src/header/header.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react';
 import './style.css'
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class header extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            isSignup : false,
-            persionLogin :  ''
+            isSignup: false,
+            persionLogin: ''
         };
         console.log(props)
-        
+
         this.clickSignup = this.clickSignup.bind(this);
     }
-    clickSignup(){
+    clickSignup() {
         alert('you just click signup');
-        this.setState({isSignup : true})
+        this.setState({ isSignup: true })
         this.props.showPopupSignin(true);
     }
 
     render() {
         return (
             <div>
-                <nav className ="navbar navbar-default">
-                <div className = "navbar-header">
-                <ul className = "navbar-nav nav">
-                
-                    {this.state.isSignup ? 
-                        <p>Hello : {this.state.persionLogin}</p>
-                    :
-                    
-                        <button onClick = {this.clickSignup}>Signin</button>
-                     
-                     
-                    }
-                </ul>
-                
-                </div>
-            </nav>
+                <nav className="navbar navbar-default">
+                    <div className="navbar-header">
+                        <ul className="navbar-nav nav">
+
+                            {this.state.isSignup ?
+                                <p>Hello : {this.state.persionLogin}</p>
+                                :
+
+                                <button onClick={this.clickSignup}>Signin</button>
+
+
+                            }
+                        </ul>
+
+                    </div>
+                </nav>
+               
             </div>
         );
     }
